d83f5cedb33e76444e31f0d449e6f4be5f8e643f
Kim DoHyeon <k.dohvis@gmail.com>
2018-08-21 23:49:37 +0900

    Add iat field

--- django_jwt/utils.py
+++ django_jwt/utils.py
@@ -1,3 +1,5 @@
+import datetime
+
 from jwt import encode
 
 from django.conf import settings
@@ -6,7 +8,6 @@ from django.conf import settings
 def create_access_token(user):
     identity_field = settings.JWT['IDENTITY_FIELD']
     identity = getattr(user, identity_field)
-    return encode(
-        {identity_field: identity},
-        settings.SECRET_KEY,
-    ).decode()
+    now = datetime.datetime.utcnow()
+    payload = {identity_field: identity, 'iat': now}
+    return encode(payload, settings.SECRET_KEY).decode()
